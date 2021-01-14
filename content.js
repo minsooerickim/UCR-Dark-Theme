35eda0d92197578de2fe54efb6e091c65738de67
Minsoo Kim <minsooerickim@gmail.com>
2021-01-13 17:06:29 -0800

    todo

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
 //on load
 
+//fix up coloring on multiple websites and shades of black
 //document.querySelector('html:not(img)').style.filter = 'invert(100%)';
 
 $(function() {
     $("html").css('filter', 'invert(1) hue-rotate(180deg)');
+    //exceptions
     $("img").css('filter', 'invert(1) hue-rotate(180deg)');
+    $("a").css('filter', 'invert(1) hue-rotate(180deg)');
     $(".footer_links").css('filter', 'invert(1) hue-rotate(180deg)');
 })
